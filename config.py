c8bbdda277e1050109b6866431940e8a0b5d5809
lordjirosama <lordjiro615@gmail.com>
2026-09-05 20:19:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "3b33568aa7119719d1f37cb15b3ae587")
-API_ID = int(environ.get("API_ID", "28137469"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_OWNER = int(environ.get("BOT_OWNER", "897584437"))
-BOT_USERNAME = environ.get("BOT_USERNAME", "Nazriyarection_bot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002169575469"))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002189546391"))
-DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://sonuoj:sonuhij@cluster0.a9dkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_HASH = environ.get("API_HASH", "4e8f828046a30ec70899b523bd763fa9")
+API_ID = int(environ.get("API_ID", "38751960"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "8658849738:AAFzvmhsJJLyEx82GtXNOwr06MqlKEbE08c")
+BOT_OWNER = int(environ.get("BOT_OWNER", "7754709357"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "ProAutoReaction_bot")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1004365314045"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://tgjiro441_db_user:Tgjirosamkun@cluster0.9reqco1.mongodb.net/?appName=Cluster0")
 
 # Define default emojis list
 EMOJIS = [
